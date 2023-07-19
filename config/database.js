4498c2e9cd420c7382380590ba3a983f942bec59
SimpsonRoss <thisisrosssimpson@gmail.com>
2023-07-19 14:58:56 -0400

    Update for Heroku

--- config/database.js
+++ config/database.js
@@ -4,6 +4,10 @@ mongoose.connect(process.env.DATABASE_URL);
 	
 // shortcut to mongoose.connection object
 const db = mongoose.connection;
+
+
+//Testing this for Heroku !!!!!!!
+const mongoURI = process.env.DATABASE_URL;
 	
 db.on('connected', function() {
   console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
